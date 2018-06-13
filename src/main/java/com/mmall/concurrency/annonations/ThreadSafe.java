b3b23f162d06198655b5f9b9afcf386647b42a3e
sty <286524077@qq.com>
2018-06-13 22:35:36 +0800

    add annonations

---
+++ src/main/java/com/mmall/concurrency/annonations/ThreadSafe.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annonations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程安全的类
+ * Created by james on 2018/6/13.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+
+}
